eafce1d16f2a1bba8541b3afd4b3dae9139e8453
Archpod <archpod@gmail.com>
2021-09-15 09:07:11 +0300

    Выполнил ДЗ к Уроку 8 курса "Основы JavaScript"

---
+++ lesson_8/scripts/products.js
@@ -0,0 +1,45 @@
+'use strict';
+const products = [
+    new ProductPatern(
+        0,
+        '0.jpg',
+        'Product 1',
+        'This offer looking especially ugly is guaranteed to irritate you to a certain extent.',
+        52.00,
+    ),
+    new ProductPatern(
+        1,
+        '1.jpg',
+        'Product 2',
+        'Now check out our second offer designed specifically to make you look really awful',
+        32.90,
+    ),
+    new ProductPatern(
+        2,
+        '2.jpg',
+        'Product 3',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        82.00,
+    ),
+    new ProductPatern(
+        3,
+        '3.jpg',
+        'Product 4',
+        'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.',
+        22.69,
+    ),
+    new ProductPatern(
+        4,
+        '4.jpg',
+        'Product 5',
+        'Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.',
+        92.58,
+    ),
+    new ProductPatern(
+        5,
+        '5.jpg',
+        'Product 6',
+        'Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.',
+        12.11,
+    ),
+];
